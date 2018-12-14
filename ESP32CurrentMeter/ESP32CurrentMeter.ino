022cc88ba413df6de9783980b3586b33bfdee1ba
GGuedesAB <gustavoguedesab@gmail.com>
2018-12-14 15:01:40 -0200

    Time set up for rectangular integration to find energy.

--- ESP32CurrentMeter/ESP32CurrentMeter.ino
+++ ESP32CurrentMeter/ESP32CurrentMeter.ino
@@ -1,10 +1,69 @@
-void setup() {
+
+/*
+ *  The circuit in the schematic multiplies every 5v by 0.666, so it goes down to 3.3v
+ * and the ESP32 can safely measure analog signals acording to it's ADC specs.
+ */
+
+byte offsetPin = 35;
+byte measurePin = 34;
+int scaleFactor = 185;
+
+int getOffset (byte pin){
+  int offsetRead = analogRead(pin);
+  return offsetRead;
+}
+
+int getSensorReading (byte pin){
+  int i = 0;
+  int sensorReading = 0;
+  for (i = 0; i<999; i++){
+    sensorReading += analogRead(pin);
+  }
+  sensorReading = sensorReading/1000;
+  return sensorReading;
+}
+
+float getCurrent () {
+  int ACOffset = (getOffset(offsetPin))/2;
+  int measure = getSensorReading(measurePin);
+  //Serial.print(ACOffset);
+  //Serial.print("|");
+  //Serial.println(measure);
+  /*
+   *  voltageShift is a value that starts from 0 (wich is when there is no current
+   * running throgh the sensor) and goes on according to the variation on the sensor's
+   * terminals.
+   */
+  float voltageShift = (measure - ACOffset)*(3.3/4096);
+  //I'm not very sure why there is this negative offset, so we take it off.
+  float current = voltageShift*(1000/185) + 0.69;
+  if (current > 0){
+    current = 0;
+  }
+  //Not sure why my results were half of those I saw on the internet
+  current = -2000*current;
+  return current;
+}
+
+float calculatePower (){
+  int sampleVoltage = getOffset(offsetPin)/2;
+  float voltageIn = sampleVoltage*(5.05/2047);
+  //Serial.print(voltageIn);
+  //Serial.println(" V");
+  float currentIn = getCurrent();
+  //Serial.print(currentIn);
+  //Serial.println(" mA");
+  float instantPower = (voltageIn*currentIn)/1000;
+  return instantPower;
+}
+
+void setup (){
   Serial.begin(9600);
 }
 
-void loop() {
-  int value1 = analogRead(A0);
-  int value2 = analogRead(A1);
-  String result = String (value1)+"|"+String(value2);
-  Serial.println(result);
+void loop (){
+  float aux = calculatePower();
+  Serial.println(aux);
+  //When integrating we have to multiply the value of the power by 0.2 (for rectangular integration)
+  delay(200);
 }
